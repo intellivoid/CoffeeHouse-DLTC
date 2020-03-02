86e9677a1fe370dfec59c351aaa425bde393bdb0
netkas <netkas@intellivoid.info>
2020-03-02 14:27:06 -0300

    Updated container configurations

--- setup.py
+++ setup.py
@@ -5,11 +5,8 @@ setup(
     version='1.0.0',
     description='Deep Learning Text Classification Engine',
     url='https://github.com/Intellivoid/CoffeeHouse-DLTC',
-
-    # Author details
     author='Zi Xing Narrakas',
     author_email='netkas@intellivoid.info',
-
     classifiers=[
         #   3 - Alpha
         #   4 - Beta
@@ -18,14 +15,10 @@ setup(
         'Topic :: Text Processing',
         'Programming Language :: Python :: 3',
     ],
-
-    # What does your project relate to?
     keywords='multi-label classification nlp neural networks deep learning',
-
     packages=find_packages(exclude=['tests']),
-
     install_requires=[
-        'nltk~=3.4',
+        'nltk~=1.0.0',
         'numpy~=1.17',
         'scipy~=1.3.1',
         'gensim~=3.8.0',
@@ -33,11 +26,11 @@ setup(
         'keras~=2.2.5',
         'h5py~=2.9',
         'tensorflow~=1.14.0',
-        'six'
+        'six',
+        'cython'
     ],
-
     entry_points='''
         [console_scripts]
-        magpie=magpie.linear_classifier.cli:cli
+        coffeehouse_dltc=coffeehouse_dltc.linear_classifier.cli:cli
     ''',
 )
