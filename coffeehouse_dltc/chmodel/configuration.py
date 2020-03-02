2bf44ca6359b76f9ad27953c1351cac16ca74e6a
netkas <netkas@intellivoid.info>
2020-03-02 18:15:50 -0300

    Minor bug fixes, added more detailed logging

--- coffeehouse_dltc/chmodel/configuration.py
+++ coffeehouse_dltc/chmodel/configuration.py
@@ -35,37 +35,60 @@ class Configuration(object):
                     pass
             return i + 1
         else:
-            raise ValueError("The classification label '{0}' is not defined in the configuration".format(classification_name))
+            raise ValueError(
+                "The classification label '{0}' is not defined in the configuration".format(classification_name))
 
     def classifier_contents(self, classification_name):
         if classification_name in self.classifications:
             with open(self.classifications[classification_name], 'r', encoding="utf8") as f:
                 return f.read().splitlines()
         else:
-            raise ValueError("The classification label '{0}' is not defined in the configuration".format(classification_name))
+            raise ValueError(
+                "The classification label '{0}' is not defined in the configuration".format(classification_name))
 
     def create_structure(self):
+        print("Preparing structure directory")
         temporary_path = "{0}_tmp".format(self.src)
         if path.exists(temporary_path):
             shutil.rmtree(temporary_path)
 
         data_path = path.join(temporary_path, "_data")
+        labels_file = open(path.join(temporary_path, "labels.txt"), "w+", encoding="utf8")
         os.mkdir(temporary_path)
+        print("Created directory '{0}'".format(temporary_path))
         os.mkdir(data_path)
+        print("Created directory '{0}'".format(data_path))
 
         classifier_labels = []
-        for classifier_name, classifier_data_file in self.classifications:
+        is_first = True
+
+        print("Processing classifiers")
+        for classifier_name, classifier_data_file in self.classifications.items():
             classifier_labels.append(classifier_name)
             contents = self.classifier_contents(classifier_name)
+            if not is_first:
+                labels_file.write("\n{0}".format(classifier_name))
+            else:
+                labels_file.write(classifier_name)
+            print("Created label '{0}'".format(classifier_name))
 
             current_value = 0
             for value in contents:
+                print("Processing {0}:{1}".format(classifier_name, current_value))
                 content_file_path = "{0}_{1}.txt".format(classifier_name, current_value)
                 label_file_path = "{0}_{1}.lab".format(classifier_name, current_value)
-                with open(path.join(data_path, content_file_path), "w+") as content_file:
+                with open(path.join(data_path, content_file_path), "w+", encoding="utf8") as content_file:
                     content_file.write(value)
                     content_file.close()
-                with open(path.join(data_path, label_file_path), "w+") as label_file:
+                print("Wrote file {0}".format(path.join(data_path, content_file_path)))
+                with open(path.join(data_path, label_file_path), "w+", encoding="utf8") as label_file:
                     label_file.write(classifier_name)
                     label_file.close()
+                print("Wrote file {0}".format(path.join(data_path, label_file_path)))
                 current_value += 1
+
+        print("Wrote file {0}".format(path.join(temporary_path, "labels.txt")))
+        labels_file.close()
+
+        print("Structure created at '{0}'".format(temporary_path))
+        return temporary_path
