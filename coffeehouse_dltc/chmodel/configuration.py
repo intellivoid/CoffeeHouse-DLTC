6b192c8f8cfd6682c45d0fb43f232a631bf7ed8a
netkas <netkas@intellivoid.info>
2020-03-02 18:44:18 -0300

    Updated file structures

--- coffeehouse_dltc/chmodel/configuration.py
+++ coffeehouse_dltc/chmodel/configuration.py
@@ -2,6 +2,7 @@ import os
 import json
 import shutil
 from os import path
+from coffeehouse_dltc import DLTC
 
 
 class Configuration(object):
@@ -22,7 +23,6 @@ class Configuration(object):
         self.__author__ = self.configuration['model']['author']
         self.__version__ = self.configuration['model']['version']
         self.__description__ = self.configuration['model']['description']
-        self.epochs = self.configuration['training_properties']['epoch']
 
         self.classifications = {}
         for classification_method in self.configuration['classification']:
@@ -46,19 +46,26 @@ class Configuration(object):
             raise ValueError(
                 "The classification label '{0}' is not defined in the configuration".format(classification_name))
 
+    def classifier_labels(self):
+        classifier_labels = []
+        for classifier_name, classifier_data_file in self.classifications.items():
+            classifier_labels.append(classifier_name)
+        return classifier_labels
+
     def create_structure(self):
         print("Preparing structure directory")
-        temporary_path = "{0}_tmp".format(self.src)
+        temporary_path = "{0}_data".format(self.src)
         if path.exists(temporary_path):
             shutil.rmtree(temporary_path)
 
-        data_path = path.join(temporary_path, "_data")
+        data_path = path.join(temporary_path, os.path.dirname(temporary_path))
         os.mkdir(temporary_path)
         print("Created directory '{0}'".format(temporary_path))
         os.mkdir(data_path)
         print("Created directory '{0}'".format(data_path))
 
-        labels_file = open(path.join(temporary_path, "labels.txt"), "w+", encoding="utf8")
+        labels_file_name = "{0}.labels".format(os.path.dirname(temporary_path))
+        labels_file = open(path.join(temporary_path, labels_file_name), "w+", encoding="utf8")
         classifier_labels = []
         is_first = True
 
@@ -70,25 +77,59 @@ class Configuration(object):
                 labels_file.write("\n{0}".format(classifier_name))
             else:
                 labels_file.write(classifier_name)
-            print("Created label '{0}'".format(classifier_name))
+            print("Processing label '{0}'".format(classifier_name))
 
             current_value = 0
             for value in contents:
-                print("Processing {0}:{1}".format(classifier_name, current_value))
                 content_file_path = "{0}_{1}.txt".format(classifier_name, current_value)
                 label_file_path = "{0}_{1}.lab".format(classifier_name, current_value)
                 with open(path.join(data_path, content_file_path), "w+", encoding="utf8") as content_file:
                     content_file.write(value)
                     content_file.close()
-                print("Wrote file {0}".format(path.join(data_path, content_file_path)))
                 with open(path.join(data_path, label_file_path), "w+", encoding="utf8") as label_file:
                     label_file.write(classifier_name)
                     label_file.close()
-                print("Wrote file {0}".format(path.join(data_path, label_file_path)))
                 current_value += 1
+            print("Processed label '{0}'".format(classifier_name))
 
-        print("Wrote file {0}".format(path.join(temporary_path, "labels.txt")))
+        print("Wrote file {0}".format(path.join(temporary_path,  labels_file_name)))
         labels_file.close()
 
         print("Structure created at '{0}'".format(temporary_path))
         return temporary_path
+
+    def train_model(self):
+        directory_structure = self.create_structure()
+
+        print("Preparing output directory")
+        output_path = "{0}_output".format(self.src)
+
+        embeddings_path = path.join(output_path, "embeddings")
+        scaler_path = path.join(output_path, "scaler")
+        model_file_path = path.join(output_path, "model.chm")
+        labels_file_path = path.join(output_path, "labels.json")
+
+        if path.exists(output_path):
+            shutil.rmtree(output_path)
+
+        os.mkdir(output_path)
+
+        print("Training model")
+        # noinspection SpellCheckingInspection
+        dltc = DLTC()
+        dltc.init_word_vectors(
+            directory_structure,
+            vec_dim=self.configuration['training_properties']['vec_dim']
+        )
+        dltc.train(
+            directory_structure,
+            self.classifier_labels(), epochs=self.configuration['training_properties']['epoch']
+        )
+
+        print("Saving data to disk")
+        dltc.save_word2vec_model(embeddings_path)
+        dltc.save_scaler(scaler_path)
+        dltc.save_model(model_file_path)
+        with open(labels_file_path, 'w', encoding='utf-8') as f:
+            json.dump(self.classifier_labels(), f, ensure_ascii=False, indent=4)
+        print("Done")
