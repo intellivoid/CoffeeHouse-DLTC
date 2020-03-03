01b3325ccbccd5cb56c36c787901b4574503adab
netkas <netkas@intellivoid.info>
2020-03-03 14:09:17 -0300

    Minor correction

--- coffeehouse_dltc/__main__.py
+++ coffeehouse_dltc/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
 import coffeehouse_dltc
+import sys
 
 if __name__ == '__main__':
-    coffeehouse_dltc.main()
+    coffeehouse_dltc.main(sys.argv)
